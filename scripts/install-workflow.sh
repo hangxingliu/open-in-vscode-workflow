1001cc166f103adb4dc7ef6b01576ab818e16a38
Liu Yue <hangxingliu@gmail.com>
2022-11-13 06:59:30 +0800

    fix install script (#1)

--- scripts/install-workflow.sh
+++ scripts/install-workflow.sh
@@ -6,12 +6,30 @@ pushd "$( dirname "${BASH_SOURCE[0]}" )/.." || exit 1;
 ./node_modules/.bin/tsc || exit 1;
 
 FROM_DIR="./workflow";
-ALFRED4_WORKFLOWS_DIR="$HOME/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows"
 
-[ -d "$ALFRED4_WORKFLOWS_DIR" ] || throw "'$ALFRED4_WORKFLOWS_DIR' is not a directory!";
+ALFRED_BASE_DIR="${HOME}/Library/Application Support/Alfred";
+ALFRED_WORKFLOWS_DIR="${ALFRED_BASE_DIR}/Alfred.alfredpreferences/workflows";
+ALFRED_PREFS_JSON="${ALFRED_BASE_DIR}/prefs.json";
+
+function get_current_workflows_dir() {
+  env ALFRED_PREFS_JSON="$ALFRED_PREFS_JSON" node -e '
+  try {
+    const fs = require("fs");
+    const prefs = JSON.parse(fs.readFileSync(process.env.ALFRED_PREFS_JSON, "utf8"));
+    if (prefs && prefs.current) console.log(prefs.current);
+  } catch (error) {}
+  ';
+}
+_ALFRED_WORKFLOWS_DIR="$(get_current_workflows_dir)";
+if test -n "$_ALFRED_WORKFLOWS_DIR"; then
+  ALFRED_WORKFLOWS_DIR="${_ALFRED_WORKFLOWS_DIR}/workflows";
+  echo "[ ] use workflows directory path '${ALFRED_WORKFLOWS_DIR}' from prefs.json";
+fi
+
+[ -d "$ALFRED_WORKFLOWS_DIR" ] || throw "'$ALFRED_WORKFLOWS_DIR' is not a directory!";
 [ -d "$FROM_DIR" ] || throw "'$FROM_DIR' is not a directory!";
 
-TARGET_DIR="${ALFRED4_WORKFLOWS_DIR}/vscode"
+TARGET_DIR="${ALFRED_WORKFLOWS_DIR}/vscode"
 if test -d "$TARGET_DIR"; then
   rm -r "$TARGET_DIR" || exit 1;
 fi
