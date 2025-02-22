26f25f93de3ef7eca9c8b18e3a221415fbb7fea3
Liu Yue <hangxingliu@gmail.com>
2025-02-22 10:47:01 +0800

    feat: Add more project file names into the scanning list

--- src/alfred/config-defaults.ts
+++ src/alfred/config-defaults.ts
@@ -8,23 +8,38 @@ export const SCAN_PRUNING_NAME: ReadonlyArray<string | RegExp> = [
 ];
 
 export const SCAN_PROJECT_FILES: ReadonlyArray<string> = [
+  // version control system directories
   '.git',
   '.svn',
   '.hg',
-  //
+  // IDE config directories
   '.vscode',
   '.idea',
-  //
-  'package.json',
-  'requirements.txt',
-  'Cargo.toml',
-  'go.mod',
-  //
+  // Common project files
   'Makefile',
   'README.md',
+  // Node.js
+  'package.json',
+  // Java
+  'gradle.properties',
+  // Python
+  'requirements.txt',
+  'pyproject.toml',
+  // Rust
+  'Cargo.toml',
+  // Golang
+  'go.mod',
+  // C/C++
+  'CMakeLists.txt',
+  // Ruby
+  'Gemfile',
+  // PHP
+  'composer.json',
+  // C#/Visual Studio
+  // '*.sln'
 ];
 
 export const CUSTOM_PREFIXES = {
   '/': '/',
   '~/': '~/',
-};
+} as const;
