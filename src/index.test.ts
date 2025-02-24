e312ba58e83f093b91be16fd62ba9ae6a1b79893
Liu Yue <hangxingliu@gmail.com>
2025-02-25 01:07:32 +0800

    test: Update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,8 +2,10 @@ import { AlfredResult } from './alfred/result.js';
 import { main } from './index.js';
 import { loadDevEnv } from './tests/utils.js';
 
+const DEFAULT_INPUT = 'open-in';
+
 loadDevEnv();
-main({ input: 'open' })
+main({ input: process.argv[2] || DEFAULT_INPUT })
   .then((result) => {
     AlfredResult.debugResult(result);
   })
--- src/scanner/scanner.test.ts
+++ src/scanner/scanner.test.ts
@@ -6,25 +6,43 @@ import { defaultVariety } from '../vscode-varieties.js';
 import { WithPathPrefixScanner } from './scan-with-path-prefix.js';
 import { homedir } from 'os';
 import { loadDevEnv } from '../tests/utils.js';
+import { getMatchingScore } from '../match/get-score.js';
+import { AlfredInputArg } from '../alfred/input-argument.js';
 
 loadDevEnv();
-main3();
+main2();
 
 async function main() {
   const scanner = new VSCodeWorkspaceScanner(new URLSet(), defaultVariety.configDir);
   const items = await scanner.scan();
   items.forEach((item) => {
     if (item.url.protocol === 'file:') return;
-    console.log(item.remoteName, item.shortName)
-    console.log(item.url.toString())
+    console.log(item.remoteName, item.shortName);
+    console.log(item.url.toString());
   });
 }
 
 async function main2() {
+  delete process.env.config_more_projects_dir;
   const config = AlfredConfig.get();
   const scanner = new ProjectDirectoryScanner(new URLSet(), config.scanDirectory!);
   await scanner.scan();
-  console.log(scanner.result);
+
+  const result = scanner.result;
+  if (process.argv[2]) {
+    const input = AlfredInputArg.get(process.argv[2]);
+    result.forEach((it) => {
+      Object.assign(it, {
+        score: getMatchingScore(
+          it.baseName.toLowerCase(),
+          input.strLC,
+          input.getSegments().fragmentsLC,
+          50
+        ),
+      });
+    });
+  }
+  console.log(result);
 }
 
 async function main3() {
